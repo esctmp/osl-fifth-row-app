33092be79c091788a2b527deeffda1c1322bbbe5
Chun Mun <chunmun5@gmail.com>
2023-06-15 17:30:56 +0800

    Updated static page of FifthRowExco View

--- frontend/src/pages/FifthRow/EPF/View.js
+++ frontend/src/pages/FifthRow/EPF/View.js
@@ -1,9 +1,30 @@
 import React from "react";
 
+import { Card, CardContent, Box, Typography } from "@material-ui/core";
+
+import ExTable from "../../../components/CM_Components/views/dashboards/dashboard1-components/ExTable";
+
 const EPFView = () => {
 
   return (
     <div>
+      <Box>
+      <Card variant="outlined">
+        <CardContent>
+          <Typography variant="h3">Basic Table</Typography>
+          <Box
+            sx={{
+              overflow: {
+                xs: "auto",
+                sm: "unset",
+              },
+            }}
+          >
+            <ExTable />
+          </Box>
+        </CardContent>
+      </Card>
+    </Box>
     </div>
   );
 };
