3f6a65912f621984db39f38dafeab3455f660a8d
Chun Mun <chunmun5@gmail.com>
2023-06-29 20:56:01 +0800

    Updated links and filter by buttons

--- frontend/src/pages/FifthRow/EPF/View.js
+++ frontend/src/pages/FifthRow/EPF/View.js
@@ -3,7 +3,7 @@ import React from "react";
 import { Card, CardContent, Box, Typography } from "@material-ui/core";
 
 import ExTable from "../../../components/CM_Components/views/dashboards/dashboard1-components/ExTable";
-import { DefaultButtonGroup } from "../../../components/CM_Components/DefaultButtonGroup";
+import { EXCO_DefaultButtonGroup } from "../../../components/CM_Components/EXCO_DefaultButtonGroup";
 
 const EPFView = () => {
 
@@ -15,7 +15,7 @@ const EPFView = () => {
           <Typography variant="h3">Archives</Typography>
           <Box sx={{ display: 'flex', alignItems: 'center' }}>
             <Box sx={{ marginLeft: 'auto' }}>
-              <DefaultButtonGroup />
+              <EXCO_DefaultButtonGroup />
             </Box>
           </Box>
          
