60d3ae4335edc4baddce07d181a48c7dede38a02
Lim Jun Kiat <limjunkiat@Joes-Macbook-Air.local>
2023-07-19 21:47:44 +0800

    auth+user_id made global

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { useRoutes } from "react-router-dom";
 import { ThemeProvider } from "@material-ui/core";
 import { baseTheme } from './assets/global/Theme-variable'
+import {UserID} from "./routes/UserID"
 import Themeroutes from "./routes/Router";
 const App = () => {
+  const [userId,setUserId] = useState(null);
   const routing = useRoutes(Themeroutes);
   const theme = baseTheme;
   return (
+    <UserID.Provider value ={{userId,setUserId}}>
     <ThemeProvider theme={theme}>
       {routing}
     </ThemeProvider>
+    </UserID.Provider>
   );
 };
 
