ae8a01d233a5300cbe9787cfe741c99321d8c062
cococoolbean <cococoolbeana@gmail.com>
2023-07-27 17:36:10 +0800

    feat: remove backgup

--- frontend/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ frontend/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,13 +1,1 @@
-export type AmplifyDependentResourcesAttributes = {
-  "auth": {
-    "frontendd0b142b8": {
-      "AppClientID": "string",
-      "AppClientIDWeb": "string",
-      "IdentityPoolId": "string",
-      "IdentityPoolName": "string",
-      "UserPoolArn": "string",
-      "UserPoolId": "string",
-      "UserPoolName": "string"
-    }
-  }
-}
+export type AmplifyDependentResourcesAttributes = {}
