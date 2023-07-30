7815eb71a5fba6331301985be4f46fcb248a9e50
cococoolbean <cococoolbeana@gmail.com>
2023-07-30 11:47:15 +0800

    feat: add reset password

--- frontend/src/routes/Router.js
+++ frontend/src/routes/Router.js
@@ -9,6 +9,7 @@ const FullLayout = lazy(() => import("../layouts/FullLayout/FullLayout.js"));
 const Dashboard1 = lazy(() => import("../views/dashboards/Dashboard1"));
 const Login = lazy(() => import("../pages/Shared/Login.js"));
 const Setting = lazy(() => import("../pages/Shared/Setting.js"));
+const Reset = lazy(() => import("../pages/Shared/ResetPassword.js"));
 const OSLCreateUser = lazy(() => import("../pages/OSL/CreateUser.js"));
 const ForgetPassword = lazy(() => import("../pages/Shared/ForgetPassword.js"));
 const FifthRowHomepage = lazy(() => import("../pages/FifthRow/Homepage"));
@@ -48,6 +49,7 @@ const ThemeRoutes = [
   { path: "/login",  element: <Login /> },
   { path: "/forgetpassword", element: <ForgetPassword />},
   { path: "/Setting", element: <Setting />},
+  { path: "/reset-password", element: <Reset/>},
   {
     path: "/",
     element: <FullLayout />,
