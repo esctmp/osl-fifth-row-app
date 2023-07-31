bb0b6abca226bc9dbb83b78526ae57971c58aa2b
Gizelle Lim <gizelle9lim@gmail.com>
2023-08-01 02:47:44 +0800

    feat: show only respective user group's nav bar

---
+++ frontend/src/layouts/FullLayout/Sidebar/FREitems.js
@@ -0,0 +1,22 @@
+const FREitems = [
+  {
+    title: "FifthRow_Homepage",
+    href: "fifthrow/homepage",
+  },
+  {
+    title: "FifthRow_EPF",
+    href: "fifthrow/epf",
+  },
+  {
+    title: "FifthRow_Create New EPF",
+    href: "fifthrow/epf/new",
+    type: "sub-heading"
+  },
+  {
+    title: "FifthRow_View EPF List",
+    href: "fifthrow/epf/view",
+    type: "sub-heading"
+  },
+]
+
+export default FREitems;
---
+++ frontend/src/layouts/FullLayout/Sidebar/OSLitems.js
@@ -0,0 +1,22 @@
+const OSLitems = [  
+    {
+      title: "OSL_Homepage",
+      href: "osl/homepage",
+    },
+    {
+      title: "OSL_EPF",
+      href: "osl/epf",
+    },
+    {
+      title: "OSL_Review EPF",
+      href: "osl/epf/submit",
+      type: "sub-heading"
+    },
+    {
+      title: "OSL_View EPF List",
+      href: "osl/epf/view",
+      type: "sub-heading"
+    },
+  ]
+
+  export default OSLitems;
