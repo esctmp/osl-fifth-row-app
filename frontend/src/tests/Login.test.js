11db8d65df63706769399517221ff1d4a6113422
jk <limjunkiat5@gmail.com>
2023-08-01 22:44:27 +0800

    added more tests

--- frontend/src/tests/Login.test.js
+++ frontend/src/tests/Login.test.js
@@ -28,6 +28,26 @@ describe("Login Form Validation",() => {
         expect(errorMessage).toBeInTheDocument();
         
     })
+    test("should reject the log in due to invalid email, not in the form of ...@...sutd.edu.sg",async()=>{
+        render(<Router>
+            <UserID.Provider value ={{userId:'null',setUserId:()=>{}}}>
+            <Groups.Provider value ={{groups:'null',setGroups:()=>{}}}>
+                <Login/>
+            </Groups.Provider>
+            </UserID.Provider>
+            </Router>);
+            const emailField = screen.getByPlaceholderText("Enter your club email");
+            const passwordField = screen.getByPlaceholderText("Enter your password");
+            const submit = screen.getByTestId("Log in");
+            await act(async()=>{
+                fireEvent.change(emailField,{target:{value:"test@gmail.com"}});
+                fireEvent.change(passwordField,{target:{value:"P@ssword1!"}});
+                fireEvent.click(submit);
+            });
+            const errorMessage  = await screen.queryByText("*Invalid email address");
+            expect(errorMessage).not.toBeInTheDocument();
+        
+    })
     test("should not have an error in the login field if login field is filled in", async()=>{
         render(<Router>
             <UserID.Provider value ={{userId:'null',setUserId:()=>{}}}>
@@ -56,16 +76,37 @@ describe("Login Form Validation",() => {
             </Groups.Provider>
             </UserID.Provider>
             </Router>);
-        const passwordField = screen.getByPlaceholderText("Enter your password");
         const emailField = screen.getByPlaceholderText("Enter your club email");
+        const passwordField = screen.getByPlaceholderText("Enter your password");
         const submit = screen.getByTestId("Log in");
         await act(async () => {
-            fireEvent.change(emailField,{target:{value:"testfre@club.sutd.edu.sg"}})
+            fireEvent.change(emailField,{target:{value:"testfre@club.sutd.edu.sg"}});
             fireEvent.change(passwordField, { target: { value: "" } });
             fireEvent.click(submit);
           });
         const errorMessage = await screen.queryByText("*Password is required!")
         expect(errorMessage).toBeInTheDocument();
     })
+    test("should not have an error if password field is filled in",async()=>{
+        render(
+        <Router>
+            <UserID.Provider  value = {{userid:"null",setUserId:()=>{}}}>
+                <Groups.Provider value = {{userid:"null",setUserId:()=>{}}}>
+                    <Login/>
+                </Groups.Provider>
+            </UserID.Provider>
+        </Router>);
+        const emailField = screen.getByPlaceholderText("Enter your club email");
+        const passwordField = screen.getByPlaceholderText("Enter your password");
+        const submit = screen.getByTestId("Log in");
+        await act(async()=>{
+            fireEvent.change(emailField,{target:{value:"testfre@club.sutd.edu.sg"}});
+            fireEvent.change(passwordField,{target:{value:"P@ssword1!"}});
+            fireEvent.click(submit);
+        });
+        const errorMessage = await screen.queryByText("*Password is required!")
+        expect(errorMessage).not.toBeInTheDocument();
+    });
+
     
 });
