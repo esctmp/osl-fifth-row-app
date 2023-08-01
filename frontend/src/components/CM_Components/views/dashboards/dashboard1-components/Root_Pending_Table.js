8c41e7a502141ef00bb1d6ad881cf6c9b70d5f4b
chunmun-ops <chunmun5@gmail.com>
2023-08-01 15:17:52 +0800

    Updated Filtering & Date Sort

--- frontend/src/components/CM_Components/views/dashboards/dashboard1-components/Root_Pending_Table.js
+++ frontend/src/components/CM_Components/views/dashboards/dashboard1-components/Root_Pending_Table.js
@@ -24,6 +24,7 @@ const Root_Pending_Table = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [searchBy, setSearchBy] = useState("EPF ID");
   const [products, setProducts] = React.useState([]);
+  const [sortOrder, setSortOrder] = useState("asc");
 
   // Function to handle search input change
   const handleSearchChange = (event) => {
@@ -34,12 +35,23 @@ const Root_Pending_Table = () => {
   // Function to handle search by dropdown change
   const handleSearchByChange = (event) => {
     setSearchBy(event.target.value);
-    setSearchTerm(""); // Reset the search term when changing the search by option
     setPage(0); // Reset page when the search by option changes
   };
 
+  const sortedProducts = products.slice().sort((a, b) => {
+    const dateA = new Date(a.date);
+    const dateB = new Date(b.date);
+  
+    if (sortOrder === "asc") {
+      return dateA - dateB;
+    } else {
+      return dateB - dateA;
+    }
+  });
+
+
   // Filter products based on search term and selected search by option
-  const filteredProducts = products.filter((product) => {
+  const filteredProducts = sortedProducts.filter((product) => {
     if (searchTerm === "") return true;
 
     switch (searchBy) {
@@ -49,12 +61,12 @@ const Root_Pending_Table = () => {
         return product.club.toLowerCase().includes(searchTerm.toLowerCase());
       case "Name":
         return product.epf_Name.toLowerCase().includes(searchTerm.toLowerCase());
-
+      case "Status":
+        return product.status.toLowerCase().includes(searchTerm.toLowerCase());
       default:
         return true;
     }
   });
-
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
   };
@@ -107,22 +119,30 @@ const Root_Pending_Table = () => {
     fetchData();
   }, []); // Empty dependency array to ensure the effect runs only once on component mount
 
+  const handleSort = () => {
+    setSortOrder(sortOrder === "asc" ? "desc" : "asc");
+  };
+
   return (
     <div style={{ overflowX: "auto" }}> {/* Add container with overflow scrolling */}
-    <Box mb={3}>
-      <FormControl sx={{ minWidth: 120, marginRight: 2 }}>
-        <Select value={searchBy} onChange={handleSearchByChange}>
-          <MenuItem value="EPF ID">EPF ID</MenuItem>
-          <MenuItem value="Club">Club</MenuItem>
-          <MenuItem value="Name">Name</MenuItem>
-        </Select>
-      </FormControl>
-      <InputBase
-        placeholder={`Search by ${searchBy}`}
-        value={searchTerm}
-        onChange={handleSearchChange}
-      />
-    </Box>
+    <Box mb={3}
+    sx={{ display: 'flex', alignItems: 'center' }}>
+      <Box sx={{ marginLeft: 'auto' }}>
+        <FormControl sx={{ minWidth: 120, marginRight: 2 }}>
+          <Select value={searchBy} onChange={handleSearchByChange}>
+            <MenuItem value="EPF ID">EPF ID</MenuItem>
+            <MenuItem value="Club">Club</MenuItem>
+            <MenuItem value="Name">Name</MenuItem>
+            <MenuItem value="Status">Status</MenuItem>
+          </Select>
+        </FormControl>
+        <InputBase
+          placeholder={`Search by ${searchBy}`}
+          value={searchTerm}
+          onChange={handleSearchChange}
+        />
+        </Box>
+      </Box>
     <Table
       aria-label="simple table"
       sx={{
@@ -140,6 +160,9 @@ const Root_Pending_Table = () => {
           <TableCell>
             <Typography color="textSecondary" variant="h6">
               Date
+              <span onClick={handleSort} style={{ cursor: "pointer" }}>
+                {sortOrder === "asc" ? " ▲" : " ▼"}
+              </span>
             </Typography>
           </TableCell>
           <TableCell>
