67232a55b6e4fb32435bbd5ec81de1a8f6bfdbac
jk <limjunkiat5@gmail.com>
2023-08-02 22:13:19 +0800

    changed requiredauth so that context not lost even after refreshing

--- frontend/src/RequireAuth.js
+++ frontend/src/RequireAuth.js
@@ -1,29 +1,52 @@
-import React,{useContext, useEffect} from 'react';
+import React,{useContext, useEffect,useState} from 'react';
 import useLoginStatus from './useLoginStatus';
 import FullLayout from './layouts/FullLayout/FullLayout';
 import {useNavigate} from "react-router-dom";
 import {UserLoggedIn} from './routes/UserLoggedIn';
 import {Groups} from "./routes/Groups";
+import {Auth} from "aws-amplify"
+import {UserID} from "./routes/UserID"
 
-const RequireAuth = ({ children,allowedGroups }) => {
-    const {userLoggedIn} = useContext(UserLoggedIn);
-    const {groups} = useContext(Groups);
-    const nav = useNavigate();
-    console.log("Before "+ userLoggedIn);
-    useLoginStatus();
-    console.log("After "+userLoggedIn);
+const RequireAuth = ({ children, allowedGroups }) => {
+    const [authStatus, setAuthStatus] = useState('checking'); // initial state is 'checking'
+    const {groups, setGroups} = useContext(Groups);
+    const {setUserId} = useContext(UserID);
+    const navigate = useNavigate();
+  
     useEffect(() => {
-        if (!userLoggedIn) {
-            console.log("fails at userloggedin");
-            nav("/login");
-        }
-    }, [userLoggedIn, nav]);
-    if (allowedGroups && groups && allowedGroups.includes(groups[0])){
-        return <FullLayout>{children}</FullLayout>;}
-    else{
-        nav("/login");
-        return null;
+      Auth.currentAuthenticatedUser()
+        .then(user => {
+          const group = user.signInUserSession.accessToken.payload["cognito:groups"];
+          const uid = user.username;
+          setGroups(group);
+          setUserId(uid);
+          setAuthStatus('logged-in'); // user is logged in, set status to 'logged-in'
+        })
+        .catch(error => {
+          console.error(error);
+          setAuthStatus('logged-out'); // user is not logged in, set status to 'logged-out'
+        });
+    }, []);
+  
+    useEffect(() => {
+      if (authStatus === 'logged-out') {
+        navigate('/login');
+      } else if (authStatus === 'logged-in' && !(allowedGroups && groups && allowedGroups.includes(groups[0]))) {
+        navigate('/login');
+      }
+    }, [authStatus, allowedGroups, groups, navigate]);
+  
+    // Wait for the auth status to be either 'logged-in' or 'logged-out'
+    if (authStatus === 'checking') {
+      return null;
     }
-};
-
-export default RequireAuth;
+  
+    // The rest of your component...
+    if (authStatus === 'logged-in' && allowedGroups && groups && allowedGroups.includes(groups[0])){
+      return <FullLayout>{children}</FullLayout>;
+    } else {
+      return null;
+    }
+  };
+  
+  export default RequireAuth;
