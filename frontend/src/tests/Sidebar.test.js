a51476a178898546cd5fa19b3365a7a14bb23046
Gizelle Lim <gizelle9lim@gmail.com>
2023-08-03 16:48:51 +0800

    some sidebar edits

--- frontend/src/tests/Sidebar.test.js
+++ frontend/src/tests/Sidebar.test.js
@@ -1,4 +1,22 @@
 import '@testing-library/jest-dom';
+import '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+// import mediaQuery from 'css-mediaquery';
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Sidebar from '../layouts/FullLayout/Sidebar/Sidebar';
+import { Groups } from "../routes/Groups";
+
+
+// function createMatchMedia(width) {
+//     return (query) => ({
+//       matches: mediaQuery.match(query, {
+//         width,
+//       }),
+//       addListener: () => {},
+//       removeListener: () => {},
+//     });
+//   }
 
 const userPool = 
 [
@@ -26,39 +44,95 @@ const userPool =
 ]
 
 describe ('Sidebar', () => {
+
+    // beforeAll(() => {
+    //     window.matchMedia = createMatchMedia(window.innerWidth);
+    //   });
+
     // test('Render - Sidebar', () => {
     //     render(
-    //         <UserID.Provider value ={{userId:'null', setUserId:()=>{}}}>
+    //         <Router>
     //         <Groups.Provider value ={{groups:'null', setGroups:()=>{}}}>
-    //         <Sidebar />
-    //         </Groups.Provider>
-    //         </UserID.Provider>
+    //         {/* <ThemeProvider theme={theme}> */}
+    //         <Sidebar 
+    //             user={userPool.find(user => user['type'] === "FRE")}
+    //         />
+    //         {/* </ThemeProvider> */}
+    //         </Groups.Provider> 
+    //         </Router>
     //         )
-    //     const field = screen.getByText("Dashboard");
+    //     // const field = screen.getByTestId("sidebar");
+    //     const field = screen.getByTestId("logo");
     //     expect(field).toBeInTheDocument();
     // });
 
 
+    it('should render a list item with NavLink', () => {
+    // Define the test data
+    const item = {
+        title: "Homepage",
+        href: "fifthrow/homepage",
+    };
+    const pathDirect = 'fifthrow/homepage'; // Set a path for comparison
+
+    // Render the component with necessary context and props
+    render(
+        <Router>
+        <Groups.Provider value ={{groups:'null', setGroups:()=>{}}}>
+        <Sidebar
+          item={item}
+          pathDirect={pathDirect}
+          handleClick={() => {}}
+          user={userPool.find(user => user['type'] === "FRE")}
+        />
+        </Groups.Provider>
+        </Router>
+    );
+
+    // // Get the rendered list item element
+    const list = screen.getByRole('generic');
+    // const listItem = list.querySelectorAll('li');
+
+    // // Assertions
+    // expect (listItem.length).toBe(4); 
+    // expect(listItem).toBeInTheDocument();
+//     expect(listItem).toHaveTextContent('Homepage'); // Make sure the text is rendered
+//     expect(listItem).toHaveAttribute('href', 'fifthrow/homepage'); // Ensure NavLink has the correct 'to' prop
+  });
+
+
+
     // side bar will show what buttons based on user type
     // test('Render - FRE Sidebar', () => {
-    //     render(<Router>
-    //         <UserID.Provider value ={{userId:'null', setUserId:()=>{}}}>
-    //         {/* <Groups.Provider value ={{groups:'null', setGroups:()=>{}}}> */}
-    //         <Sidebar />
-    //         {/* </Groups.Provider> */}
-    //         </UserID.Provider>
+    //     render(
+    //         <Router>
+    //         <Groups.Provider value ={{groups:'null', setGroups:()=>{}}}>
+    //         <Sidebar 
+    //             user={userPool.find(user => user['type'] === "FRE")}
+    //         />
+    //         </Groups.Provider> 
     //         </Router>
     //         )
-    //     const user = userPool.find(user => user['type'] === "FRE");
-    //     const field = screen.getByText("Homepage");
-    //     const field2 = screen.getByText("EPF");
-    //     const field3 = screen.getByText("Create New EPF");
-    //     const field4 = screen.getByText("View EPF List");
-    //     expect(field).toBeInTheDocument();
+    //     // const user = userPool.find(user => user['type'] === "FRE");
+    //     const list = screen.getByRole('generic');
+    //     expect (list).toBeInTheDocument();
+    //     // -------- works until here
+    //     const listItem = list.querySelectorAll('li');
+    //     expect(listItem).not.toBe("[]")
+        // expect(listItem.length).toBeGreaterThan(0);
+        // expect (listItem).toHaveTextContent("Homepage");
+        // const field1 = screen.getByTestId("Homepage");
+        // const field2 = screen.getByText("EPF");
+        // const field3 = screen.getByText("Create New EPF");
+        // const field4 = screen.getByText("View EPF List");
+        // expect(field1).toBeInTheDocument();
     //     expect(field2).toBeInTheDocument();
     //     expect(field3).toBeInTheDocument();
     //     expect(field4).toBeInTheDocument();
     // });
 
     // each button will route to where it is supposed to
-})
+
+
+
+});
