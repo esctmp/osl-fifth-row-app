d76270e9a4a3c7e6858ab88bc0a7170f7bb142e6
cococoolbean <cococoolbeana@gmail.com>
2023-08-04 11:49:08 +0800

    feat: add api

--- frontend/src/pages/Root/Homepage.js
+++ frontend/src/pages/Root/Homepage.js
@@ -12,7 +12,7 @@ const Homepage = () => {
   const[EPFcount, setEPFcount] = useState("_");
   const {userId,setUserId} = useContext(UserID);
   useEffect(()=>
-  axios.get(`http://localhost:3000/users/getUser?user_id=${userId}`).then(function(response){
+  axios.get(`hhttps://mtdlypyeyk.execute-api.ap-southeast-1.amazonaws.com/staging/users/getUser?user_id=${userId}`).then(function(response){
     // console.log(response.data[0].name);
     // console.log(response.data[0].outstanding_epf);
     setEPFcount(response.data[0].outstanding_epf);
