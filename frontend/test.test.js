10fadf0aa95a6f6c9dc36ba0f47df7b5080013df
SausageMuffins <rwang9098@gmail.com>
2023-08-05 15:26:46 +0800

    testing CI-CD

---
+++ frontend/test.test.js
@@ -0,0 +1,13 @@
+describe("Test Suite", () => {
+    beforeAll(async () => {});
+
+    beforeEach(async () => {});
+
+    // Correct input cases
+    test("Test ID: 1 Test", async () => {
+        const message = "Hi";
+        expect(message).toMatch("Hi");
+    });
+
+    afterAll(async () => {});
+});
